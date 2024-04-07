39889aed2b7474643d0b48a2bc3e44d03e7c941f
rafael <yhrapha@gmail.com>
2024-04-07 19:10:28 +0100

    veriscan sever commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import cors from 'cors'
+import TeachableMachine from '@sashido/teachablemachine-node'
+import dotenv from 'dotenv'
+import multer from 'multer'
+import fs from 'fs'
+
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 8880
+const model = new TeachableMachine({
+  modelUrl: process.env.TMURL
+})
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+   return cb(null, './public');
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}_${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage });
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res)=>{
+  res.send(`<h4>Server is running </h4>`)
+})
+
+app.post('/deepfake',upload.single('file'), async (req, res)=>{
+
+  try {
+    
+    const image = req.file.path
+    // console.log(image)
+    const imageData = fs.readFileSync(image)
+    // console.log(imageData.toString('base64'))
+
+    const dataurl = `data:image/jpeg;base64,${imageData.toString('base64')}`
+
+    const predictions = await model.classify({
+       imageUrl: dataurl
+    })
+  console.log(predictions)
+
+  } catch (error) {
+    console.log(error)
+  }
+
+
+  
+
+
+})
+
+app.listen(PORT, ()=>{
+  console.log(`server is running on port ${PORT}...`)
+})
