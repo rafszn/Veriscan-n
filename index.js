ecc7ed5d5aa7ce3925445d5991915013e898a6e5
rafael <yhrapha@gmail.com>
2024-09-03 18:08:31 +0100

    fix

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-import express from 'express'
-import cors from 'cors'
-import TeachableMachine from '@sashido/teachablemachine-node'
-import dotenv from 'dotenv'
-import multer from 'multer'
-import fs from 'fs'
-dotenv.config()
+import express from "express";
+import cors from "cors";
+import TeachableMachine from "@sashido/teachablemachine-node";
+import dotenv from "dotenv";
+import multer from "multer";
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 8880
+const app = express();
+const PORT = process.env.PORT || 8880;
 
 const model = new TeachableMachine({
-  modelUrl: process.env.TMURL
-})
+  modelUrl: process.env.TMURL,
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-   return cb(null, '/tmp');
+    return cb(null, "/tmp");
   },
   filename: function (req, file, cb) {
     return cb(null, `${Date.now()}_${file.originalname}`);
-  }
+  },
 });
 
 const upload = multer({ dest: "/tmp" });
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send(`<h4>Server is running </h4>`);
+});
 
-app.get('/', (req, res)=>{
-  res.send(`<h4>Server is running </h4>`)
-})
-
-app.post('/deepfake',upload.single('file'), async (req, res)=>{
+app.post("/deepfake", upload.single("file"), async (req, res) => {
+  cloudinary.config({
+    cloud_name: "dic7urzye",
+    api_key: "239763643334781",
+    api_secret: process.env.CLOUD_SECRET,
+  });
 
   try {
-    const image = req.file.path
-    const imageData = fs.readFileSync(image)
-    const dataurl = `data:image/jpeg;base64,${imageData.toString('base64')}`
+    const { path } = req.file;
+    const imageData = fs.readFileSync(path);
+    const dataurl = `data:image/jpeg;base64,${imageData.toString("base64")}`;
+
+    const { url } = await cloudinary.uploader.upload(dataurl, {
+      public_id: "deep",
+    });
+
     const predictions = await model.classify({
-       imageUrl: dataurl
-    })
-  // console.log(predictions)
-  res.status(200).json(predictions)
-
+      imageUrl: url,
+    });
+    res.status(200).json(predictions);
   } catch (error) {
-    res.status(400).json({message: 'Something went wrong'})
+    res.status(400).json({ message: "Something went wrong" });
   }
-})
+});
 
-app.listen(PORT, ()=>{
-  console.log(`server is running on port ${PORT}...`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}...`);
+});
